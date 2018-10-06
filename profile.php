db23a44f7dc921f9b7223e5d402549e7851c16d3
IzzatusSakinah <wbd02_01091598@pb.edu.bn>
2018-10-06 11:53:52 +0800

    change profile

--- profile.php
+++ profile.php
@@ -25,93 +25,63 @@ include('function.php');
         ?>
 
         <!-- Top header -->
-        <div class="header-box">
-          <div class="logout-box">
-            <p class="username">Lauren William</p>
-            <p class="user-status">Student</p>
-          </div>
-          <p class="dashboardtext">Dashboard &nbsp; ▶</p>
-          <p class="profiletext">Profile</p>
-          <p class="text">Personal Details</p>
-          <div class="dropdown">
-                    <img id="img-sort" class="dropbtn" src="img/sort.png" onclick="myFunction()" style="width: 9px; height: 9px;">
-                <div id="myDropdown" class="dropdown-content">
-                    <a href="studentprof">Profile</a>
-                    <a href="logout.php">Logout</a>
-                </div>
-            </div>
-        </div>
+        <?php
+        include('header.php');
+        ?>
 
         <!-- Page Content -->
-        <div class="organize">
-                <p class="status">Status</p>
-                <p class="student">Student</p>   
-
-            <div class="userbox"> 
-                <p class="userstudent">&nbsp;&nbsp;Username&nbsp;&nbsp;</p> 
-                <input class="box" type="text" name="text">  
-            </div>
-            
-            <div class="B">
-                <div class="userbox"> 
-                    <p class="userstudent">&nbsp;&nbsp;Fullname&nbsp;&nbsp;</p>   
-                </div>
-            </div>
-
-            <div class="copy">
-                <p class="status">GroupCode</p>
-                <p class="student">DiICT(INS)0216/02</p>
-            </div>
-
-            <div class="copy1">
-                <p class="status">ID</p>
-                <p class="student">DiICT(INS)/06/16/0001</p>
-            </div>
-
-            <div class="C">
-                <div class="userbox"> 
-                    <p class="userstudent">&nbsp;&nbsp;Email&nbsp;&nbsp;</p>   
-                </div>
-            </div>
-
-            <div class="D">
-                <div class="userbox"> 
-                    <p class="userstudent">&nbsp;&nbsp;Password&nbsp;&nbsp;</p>   
-                </div>
-            </div>
-
-            <div class="save-btn">
-                <p class="save">Save Changes</p>
-            </div>
-        </div>
-
-            
-            <script>
-                    /* When the user clicks on the button, 
-                    toggle between hiding and showing the dropdown content */
-                    function myFunction() {
-                        document.getElementById("myDropdown").classList.toggle("show");
-                    }
-
-                    // Close the dropdown if the user clicks outside of it
-                    window.onclick = function(event) {
-                        if (!event.target.matches('.dropbtn')) {
-
-                            var dropdowns = document.getElementsByClassName("dropdown-content");
-                            var i;
-                                for (i = 0; i < dropdowns.length; i++) {
-                            var openDropdown = dropdowns[i];
-                                if (openDropdown.classList.contains('show')) {
-                                openDropdown.classList.remove('show');
-                            }
-                        }
-                    }
-                }
-        </script>
+        <?php
+            $query = 'SELECT * FROM user WHERE username="'.$_SESSION['username'].'"';
+            $run_query = mysqli_query($connection, $query);
+        ?>
         
+        <?php
+            foreach($run_query as $user){
+                echo"
+                <div class='organize'>
+                    <p class='status'>Status</p>
+                    <p class='student'>".$user['role']."</p>   
 
+                    <div class='userbox'> 
+                        <p class='userstudent'>&nbsp;&nbsp;Username&nbsp;&nbsp;</p> 
+                        <input class='box' type='text' name='text'>  
+                    </div>
+                    
+                    <div class='B'>
+                        <div class='userbox'> 
+                            <p class='userstudent'>&nbsp;&nbsp;Fullname&nbsp;&nbsp;</p>
+                            <p>".$user['name']."</p>   
+                        </div>
+                    </div>
 
+                    <div class='copy'>
+                        <p class='status'>GroupCode</p>
+                        <p class='student'>".$user['code']."</p>
+                    </div>
 
-        
+                    <div class='copy1'>
+                        <p class='status'>ID</p>
+                        <p class='student'>DiICT(INS)/06/16/0001</p>
+                    </div>
+
+                    <div class='C'>
+                        <div class='userbox'> 
+                            <p class='userstudent'>&nbsp;&nbsp;Email&nbsp;&nbsp;</p>
+                            <p>".$user['email']."</p>   
+                        </div>
+                    </div>
+
+                    <div class='D'>
+                        <div class='userbox'> 
+                            <p class='userstudent'>&nbsp;&nbsp;Password&nbsp;&nbsp;</p>   
+                        </div>
+                    </div>
+
+                    <div class='save-btn'>
+                        <p class='save'>Save Changes</p>
+                    </div>
+                </div>";
+            }
+        ?>
     </body>
 </html>
