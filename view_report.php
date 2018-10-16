5fda3bf98dca4fcdca897fe108f5666458c4c484
haziqahao <36142487+haziqahao@users.noreply.github.com>
2018-10-16 13:23:19 +0800

    change title

--- view_report.php
+++ view_report.php
@@ -5,11 +5,10 @@ include('function.php');
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
+    <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <title>View Report</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
